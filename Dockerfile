782ec4026bbe77ab3337f4fe4508fb1340003d52
Ndi-Shema <f.shema@alustudent.com>
2024-12-12 16:23:46 +0200

    all updated workflow

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,21 @@
-# Dockerfile
 FROM python:3.9-slim
 
 WORKDIR /app
+
+# Install system dependencies for psycopg2
+RUN apt-get update && apt-get install -y \
+    postgresql-client \
+    gcc \
+    python3-dev \
+    musl-dev \
+    && rm -rf /var/lib/apt/lists/*
 
 COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 
 COPY . /app/
+
+# Remove or comment out the collectstatic command
+# CMD ["python", "manage.py", "collectstatic", "--noinput"]
 
 CMD ["gunicorn", "--bind", "0.0.0.0:8000", "todo_api.wsgi:application"]
--- todo_api/settings.py
+++ todo_api/settings.py
@@ -98,7 +98,7 @@ DATABASES = {
         'USER': 'shema',
         'PASSWORD': 'shema',
         'HOST': 'db',
-        'PORT': '2409',
+        'PORT': '5432',  # Change this to 5432
     }
 }
 
@@ -136,10 +136,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-
-LOGIN_REDIRECT_URL = '/'
-LOGOUT_REDIRECT_URL = '/login/'
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
